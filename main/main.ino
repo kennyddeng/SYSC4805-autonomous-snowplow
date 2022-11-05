b6e0959f01d374bc430a7bb1191ec1f3f15bfec9
Kenny Deng <kennyddeng@gmail.com>
2022-11-05 17:24:26 -0400

    add: motor behaviour debug

--- main/main.ino
+++ main/main.ino
@@ -8,18 +8,18 @@ void setup() {
 
 void loop() {
     // Move forward for 5 seconds
-    moveForward(0.0);
-    delay(5000);
+//    moveForward(0.0); // currently all motors going in reverse, motor1 slow (both directions active)
+//    delay(5000);
     
-    //Move backwards for 5 seconds
-    moveBackward(0.0); 
-    delay(5000);
-
-    //turn right for 5 seconds
-    turnRight(0.0); 
-    delay(5000);
-
-    //turn left for 5 seconds
-    turnLeft(0.0); 
-    delay(5000);
+//    //Move backwards for 5 seconds
+//    moveBackward(0.0); // currently right motors going reverse, left not moving
+//    delay(5000);
+//
+//    //turn right for 5 seconds
+//    turnRight(0.0); // all motors enabled, stalled (both directions active)
+//    delay(5000);
+//
+//    //turn left for 5 seconds
+//    turnLeft(0.0); // right motors enabled, stalled (both directions active)
+//    delay(5000);
 }
